a310a382e3f28ca44f3c0aaecd1bcde60e29d5e5
Kanan <43238830+KenanBoyukkishiyev1997@users.noreply.github.com>
2018-09-28 16:26:07 +0400

    First C#
    if you want work, firstly you should put just number. If you add any items of alphabet, you will see warning.  It will work in Visual Studio. If you have not a Visual Studio, please, use  Command Promont

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("eded daxil edin");
+            int s;
+            bool parsed = int.TryParse(Console.ReadLine(), out s);
+            if (parsed)
+            {
+                s++;
+                Console.WriteLine("daxil etdiyiniz eded budu :" + s);
+            }
+            else
+            {
+                Console.WriteLine("daxil etdiyiniz ededin  :" );
+            }
+          
+               
+            
+
+        }
+    }
+}
